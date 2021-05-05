c3b47cc70ea34a1d8f2e6b4a6dd82bb375c027be
MashaNes <masa_nesic@outlook.com>
2021-05-05 13:48:14 +0200

    System initialization. Python implementation for temperature and air quality sensors added. .NET Core strusture for acqusition gateway added.

---
+++ AcquisitionGateway/AcquisitionGateway/Services/AirQualityService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcquisitionGateway.Entities;
+using AcquisitionGateway.Contracts;
+
+namespace AcquisitionGateway.Services
+{
+    public class AirQualityService : IAirQualityService
+    {
+        public async Task<bool> AddData(AirQualityData newData)
+        {
+            //send newData to Kafka topic
+            return true;
+        }
+    }
+}
---
+++ AcquisitionGateway/AcquisitionGateway/Services/TempFService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcquisitionGateway.Contracts;
+using AcquisitionGateway.Entities;
+
+namespace AcquisitionGateway.Services
+{
+    public class TempFService : ITempFService
+    {
+        public async Task<bool> AddData(RoadAndAirTempData newData)
+        {
+            newData.AirTemperature = convertTempFtoC(newData.AirTemperature);
+            newData.RoadTemperature = convertTempFtoC(newData.RoadTemperature);
+            //send newData to temperature Kafka topic
+            return true;
+        }
+
+        private double convertTempFtoC(double tempInF)
+        {
+            return (tempInF - 32) / (double)1.8;
+        }
+    }
+}
