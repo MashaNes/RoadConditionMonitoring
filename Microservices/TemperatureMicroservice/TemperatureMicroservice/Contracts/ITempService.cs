259df9e757b837317755a36c0a890ea0ab9031c2
MashaNes <masa_nesic@outlook.com>
2021-05-16 16:13:30 +0200

    Cassandra connection and test APIs added.

---
+++ Microservices/TemperatureMicroservice/TemperatureMicroservice/Contracts/ITempService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemperatureMicroservice.Entities;
+
+namespace TemperatureMicroservice.Contracts
+{
+    public interface ITempService
+    {
+        Task<bool> AddData(RoadAndAirTempData newData);
+        Task<List<RoadAndAirTempData>> GetAllData();
+        Task<RoadAndAirTempData> GetDataByRecordId(int recordId);
+        Task<List<RoadAndAirTempData>> GetDataByStationName(String StationName, bool Newest);
+        Task<List<RoadAndAirTempData>> GetDataByTimestamp(String StationName, DateTime time, int seconds);
+        //Task<List<RoadAndAirTempData>> GetDataLocation(LocationRadiusDTO locationInfo);
+    }
+}
---
+++ Microservices/TemperatureMicroservice/TemperatureMicroservice/Controllers/TempController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemperatureMicroservice.Contracts;
+using TemperatureMicroservice.Entities;
+
+namespace TemperatureMicroservice.Controllers
+{
+    [ApiController]
+    [Route("api/temp_microservice")]
+    public class TempController : ControllerBase
+    {
+        private readonly ITempService _tempService;
+
+        public TempController(ITempService tempService)
+        {
+            _tempService = tempService;
+        }
+
+        [HttpPost]
+        [Route("add-data")]
+        public async Task<ActionResult> AddData([FromBody] RoadAndAirTempData roadData)
+        {
+            await _tempService.AddData(roadData);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("get-data")]
+        public async Task<ActionResult> GetData()
+        {
+            List<RoadAndAirTempData> retVal = await _tempService.GetAllData();
+            return Ok(retVal);
+        }
+
+        [HttpGet]
+        [Route("get-data-recordId/{RecordId}")]
+        public async Task<ActionResult> GetData(int RecordId)
+        {
+            RoadAndAirTempData retVal = await _tempService.GetDataByRecordId(RecordId);
+            return Ok(retVal);
+        }
+
+        [HttpGet]
+        [Route("get-data-station/{StationName}/{Newest}")]
+        public async Task<ActionResult> GetDataStation(String StationName, bool Newest)
+        {
+            List<RoadAndAirTempData> retVal = await _tempService.GetDataByStationName(StationName, Newest);
+            return Ok(retVal);
+        }
+
+        /*[HttpPost]
+        [Route("get-data-timeframe")]
+        public async Task<ActionResult> GetDataTimeframe([FromBody] TimeframeDTO timeframe)
+        {
+            List<RoadAndAirTempData> retVal = await _tempService.GetDataByTimestamp(timeframe.StationName, timeframe.ReferentTime, timeframe.TimeframeSeconds);
+            return Ok(retVal);
+        }*/
+
+        /*
+        [HttpPost]
+        [Route("get-data-location")]
+        public async Task<ActionResult> GetDataLocation([FromBody] LocationRadiusDTO locationRadius)
+        {
+            List<RoadAndAirTempData> retVal = await _tempService.GetDataLocation(locationRadius);
+            return Ok(retVal);
+        }
+         */
+    }
+}
