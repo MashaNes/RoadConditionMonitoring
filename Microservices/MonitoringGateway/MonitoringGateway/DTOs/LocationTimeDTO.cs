d9193cd6e8c2b0ecef4bfe01b3a3f8ee5f6958a9
MashaNes <masa_nesic@outlook.com>
2021-05-19 23:46:23 +0200

    DTOs added.

---
+++ Microservices/MonitoringGateway/MonitoringGateway/DTOs/LocationTimeDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitoringGateway.DTOs
+{
+    public class LocationTimeDTO
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double RadiusMeters { get; set; }
+        public DateTime ReferenceTime { get; set; }
+        public int Seconds { get; set; }
+    }
+}
